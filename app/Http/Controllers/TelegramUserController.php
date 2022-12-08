0060ae3c30577af2d7266a901ae7ad1e6e82a510
Mikhail Abramishvili <mg@abramishvili.net>
2022-12-08 18:08:56 +0400

    .

---
+++ app/Http/Controllers/TelegramUserController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Setting;
+use App\Models\TelegramUser;
+use Illuminate\Http\Request;
+
+class TelegramUserController extends Controller
+{
+    public function check()
+    {
+        $url = "https://api.telegram.org/bot";
+        $url .= Setting::find(1);
+        $url .= "/getupdates";
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $headers = array(
+            "Accept: application/json",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response = curl_exec($curl); curl_close($curl);
+
+        return $response;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', function () {
 
 Route::get('rates', [App\Http\Controllers\RateController::class, 'index']);
 Route::get('getrate', [App\Http\Controllers\RateController::class, 'store']);
+
+Route::get('telegram-check', [App\Http\Controllers\TelegramUserController::class, 'check']);
