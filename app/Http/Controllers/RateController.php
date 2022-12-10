70a9bd0360a8393b776dd331e41456e17d4ed38d
Mikhail Abramishvili <mg@abramishvili.net>
2022-12-10 14:43:20 +0400

    .

--- app/Http/Controllers/RateController.php
+++ app/Http/Controllers/RateController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Setting;
 use App\Models\TelegramUser;
 use App\Models\Rate;
+use App\Models\KoronaRate;
+use App\Models\UnistreamRate;
 use Illuminate\Http\Request;
 
 class RateController extends Controller
@@ -13,11 +15,26 @@ class RateController extends Controller
     {
         return response()->json([
             'rates' => Rate::orderBy('created_at', 'desc')->get(),
+            'korona_rates' => KoronaRate::orderBy('created_at', 'desc')->get(),
+            'unistream_rates' => UnistreamRate::orderBy('created_at', 'desc')->get(),
             'min' => Rate::orderBy('rate', 'asc')->first(),
+            'min_korona' => KoronaRate::orderBy('rate', 'asc')->first(),
+            'min_unistream' => UnistreamRate::orderBy('rate', 'asc')->first(),
         ]);
     }
 
     public function store()
+    {
+        $this->storeRaif();
+
+        sleep(10);
+        $this->storeKorona();
+        
+        sleep(20);
+        $this->storeUnistream();
+    }
+
+    public function storeRaif()
     {
         $url = "https://www.raiffeisen.ru/oapi/currency_rate/get/?source=RCONNECT&currencies=USD";
 
@@ -43,6 +60,82 @@ class RateController extends Controller
         }
 
         $rate = new Rate();
+        $rate->rate = $responseRate;
+
+        $rate->save();
+        
+        $telegramUsers = TelegramUser::all();
+        
+        foreach($telegramUsers as $user)
+        {
+            return $this->sendMessageToTelgeram($user, $rate);
+        }
+    }
+
+    public function storeKorona()
+    {
+        $url = "https://koronapay.com/transfers/online/api/transfers/tariffs?sendingCountryId=RUS&sendingCurrencyId=810&receivingCountryId=GEO&receivingCurrencyId=840&paymentMethod=debitCard&receivingAmount=150000&receivingMethod=cash&paidNotificationEnabled=false";
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $headers = array(
+            "Accept: application/json",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response = curl_exec($curl); curl_close($curl);
+
+        $responseRate = json_decode($response)[0]->exchangeRate;
+
+        $lastRate = KoronaRate::orderBy('created_at', 'desc')->first();
+
+        if($lastRate && $lastRate->rate == $responseRate) {
+            return;
+        }
+
+        $rate = new KoronaRate();
+        $rate->rate = $responseRate;
+
+        $rate->save();
+        
+        $telegramUsers = TelegramUser::all();
+        
+        foreach($telegramUsers as $user)
+        {
+            return $this->sendMessageToTelgeram($user, $rate);
+        }
+    }
+
+    public function storeUnistream()
+    {
+        $url = "https://online.unistream.ru/card2cash/calculate?destination=GEO&amount=1&currency=USD&accepted_currency=RUB&profile=unistream";
+
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $headers = array(
+            "Accept: application/json",
+        );
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+        $response = curl_exec($curl); curl_close($curl);
+
+        $responseRate = json_decode($response)->fees[0]->acceptedAmount;
+
+        $lastRate = UnistreamRate::orderBy('created_at', 'desc')->first();
+
+        if($lastRate && $lastRate->rate == $responseRate) {
+            return;
+        }
+
+        $rate = new UnistreamRate();
         $rate->rate = $responseRate;
 
         $rate->save();
